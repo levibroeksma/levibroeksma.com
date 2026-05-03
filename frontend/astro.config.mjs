cb42a525330be8bb8101306d97fe2c8cec69906c
LeviBroeksmaMAPtm <levi.broeksma@maptm.nl>
2026-05-03 14:11:45 +0200

    Add Netlify adapter and update configuration for redirects in Astro project; include local Netlify folder in .gitignore and add new dependencies

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,11 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import netlify from "@astrojs/netlify";
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
+  redirects: {
+    "/": "/nl/",
+  },
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+
+  adapter: netlify(),
 });
